be92784d00c5f6a593e68704ed71f40b7e018519
Sam Jespersen <heyheysamj@gmail.com>
2019-09-19 13:25:44 -0700

    save file TDD

--- __tests__/document-collection.test.js
+++ __tests__/document-collection.test.js
@@ -7,9 +7,22 @@ jest.mock('../lib/files.js', () => ({
 // for setting up mock expectations
 const { readFile, writeFile, readdir } = require('../lib/files');
 const { DocumentCollection } = require('../lib/document-collection');
-const folder = './fake/path';
-const docs = new DocumentCollection(folder)
 
 describe('Document Collection', () => {
 
+  const folder = './fake/path';
+  const docs = new DocumentCollection(folder);
+  const object1 = { text: `I'm an object` };
+  writeFile.mockResolvedValue(object1);
+
+  it('save object', () => {
+    docs.save(object1)
+      .then(() => {
+        const writeCalls = writeFile.mock.calls;
+        expect(writeCalls.length).toBe(1);
+        expect(writeCalls[0][1]).toEqual(object1);
+        expect(writeCalls[0][0]).toContain(folder);
+
+      });
+  });
 });
