6e1551fbf9e5cf49761f1c282f0b8703447366a9
Sam Jespersen <heyheysamj@gmail.com>
2019-09-21 13:46:38 -0700

    index js

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const DocumentCollection = require('./lib/document-collection');
+const { DocumentCollection } = require('./lib/document-collection');
 
-const documents = new DocumentCollection(/* path to use */);
+const path = './test';
+const documents = new DocumentCollection(path);
 
 // write some code to exercise your document collection
+
+const objectToSave = {
+  text: 'value',
+  read: true
+};
+
+documents.save(objectToSave);
+
+documents.get('L-PFu3AA')
+  .then(res => {
+    console.log(res);
+  });
+
+documents.getAll()
+  .then(res => {
+    console.log('ALL', res);
+  });
